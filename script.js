7817533eae4d4259580f0ef4f0591a6b174134ab
Ashley <ashleykaguu@gmail.com>
2026-02-03 10:15:53 +0300

    dark and light themes,love letter

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ const noBtn = document.getElementById("noBtn");
 const result = document.getElementById("result");
 const gif = document.getElementById("gif");
 const message = document.getElementById("message");
+const loveLetter = document.getElementById("loveLetter");
+const themeToggle = document.getElementById("themeToggle");
 
 yesBtn.addEventListener("click", () => {
   gif.src = "https://media1.popsugar-assets.com/files/thumbor/NCsLs25M77bM1lIakRf9w3fJnQk=/fit-in/500x211/top/filters:format_auto():upscale()/2021/06/28/835/n/3019466/c2255a6960da1ccdbed969.90115868_giphy-5.gif"; // kissing gif
   message.textContent = "Yay! 💖 I knew it!";
+  setTimeout(() => {loveLetter.classList.remove("hidden"); loveLetter.classList.add("show");}, 5000);
   result.classList.remove("hidden");
 });
 
@@ -39,3 +42,13 @@ function startCountdown() {
 }
 
 startCountdown();
+// THEME SWITCH
+themeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  document.body.classList.toggle("light");
+
+  themeToggle.textContent =
+    document.body.classList.contains("dark")
+      ? "☀️ Light Mode"
+      : "🌙 Dark Mode";
+});
