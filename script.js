af3a627727ab4459a56c64f243f9a36e94eec440
Ashley <ashleykaguu@gmail.com>
2026-02-03 10:24:33 +0300

    confetti

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ const gif = document.getElementById("gif");
 const message = document.getElementById("message");
 const loveLetter = document.getElementById("loveLetter");
 const themeToggle = document.getElementById("themeToggle");
+const jsConfetti = new JSConfetti()
 
 yesBtn.addEventListener("click", () => {
   gif.src = "https://media1.popsugar-assets.com/files/thumbor/NCsLs25M77bM1lIakRf9w3fJnQk=/fit-in/500x211/top/filters:format_auto():upscale()/2021/06/28/835/n/3019466/c2255a6960da1ccdbed969.90115868_giphy-5.gif"; // kissing gif
   message.textContent = "Yay! 💖 I knew it!";
   setTimeout(() => {loveLetter.classList.remove("hidden"); loveLetter.classList.add("show");}, 5000);
   result.classList.remove("hidden");
+  jsConfetti.addConfetti()
 });
 
 noBtn.addEventListener("click", () => {
